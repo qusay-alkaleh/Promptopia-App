5188ffa2418304befb478bb8aca3d0f17f35b351
KusayAlkaleh <theonexr4@gmail.com>
2024-08-30 22:46:13 +0300

    fixed all typo....

--- app/prompt-update/page.jsx
+++ app/prompt-update/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { Suspense, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
@@ -54,15 +54,13 @@ const EditPrompt = () => {
   };
 
   return (
-    <Suspense>
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
-    </Suspense>
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
   );
 };
 
