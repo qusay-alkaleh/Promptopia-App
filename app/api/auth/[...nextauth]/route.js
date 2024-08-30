64cb03e58c172d3e904f3d45e61e7fe4a7f76d33
KusayAlkaleh <theonexr4@gmail.com>
2024-08-30 19:30:42 +0300

    updated Auth. functionality to handle user opertions

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,38 +10,39 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.email,
-    });
-
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      // check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
 
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          userName: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
-      }
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      return true;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        // if not, create a new user and save it in database
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            userName: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
   },
 });
 
